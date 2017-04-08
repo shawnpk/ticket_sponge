62eb09b065dae305ea397dddac2caef8cace350e
Shawn Kearney <shawnpk@ineptsoftware.com>
2017-04-08 08:44:44 -0700

    Add new project feature

---
+++ spec/features/creating_projects_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.feature 'Users can create new projects' do
+  scenario 'with valid attributes' do
+    visit '/'
+    click_link 'New Project'
+    fill_in 'Name', with: 'Atom.io'
+    fill_in 'Description', with: 'A text editor for everyone'
+    click_button 'Create Project'
+
+    expect(page).to have_content 'Project has been created.'
+  end
+end
