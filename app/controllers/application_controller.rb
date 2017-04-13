1842700bef61ace46b727f3543a4a16b96a77000
Shawn Kearney <shawnpk@ineptsoftware.com>
2017-04-13 12:57:20 -0700

    Add ability for the app to handle authorization errors gracefully

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery with: :exception
+
+  rescue_from Pundit::NotAuthorizedError, with: :not_authroized
+
+  private
+    def not_authroized
+      redirect_to root_path, alert: 'You aren\'t allowed to access this.'
+    end
 end
--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -9,4 +9,15 @@ RSpec.describe ProjectsController, type: :controller do
     message = 'The project you were looking for could not be found.'
     expect(flash[:alert]).to eq message
   end
+
+  it 'handles permission errors by rdirecting to a safe place' do
+    allow(controller).to receive(:current_user)
+
+    project = FactoryGirl.create(:project)
+    get :show, params: { id: project }
+
+    expect(response).to redirect_to(root_path)
+    message = 'You aren\'t allowed to access this.'
+    expect(flash[:alert]).to eq message
+  end
 end
