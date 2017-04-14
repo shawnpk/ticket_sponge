437afb0a49bffb549f9b25cfd6e2b88e9067334b
Shawn Kearney <shawnpk@ineptsoftware.com>
2017-04-14 07:34:52 -0700

    Add authorization to all controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,10 @@
 class ApplicationController < ActionController::Base
   include Pundit
-  protect_from_forgery with: :exception
 
+  after_action :verify_authorized, except: [:index], unless: :devise_controller?
+  after_action :verify_policy_scoped, only: [:index], unless: :devise_controller?
+
+  protect_from_forgery with: :exception
   rescue_from Pundit::NotAuthorizedError, with: :not_authroized
 
   private
