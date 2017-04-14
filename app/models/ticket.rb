bd716d619ffc00f525938ff717569755fb81ef6a
Shawn Kearney <shawnpk@ineptsoftware.com>
2017-04-13 19:34:18 -0700

    Allow only members of project to view tickets

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,6 +1,6 @@
 class Ticket < ApplicationRecord
   belongs_to :project
-  belongs_to :user
+  belongs_to :user, optional: true
 
   validates :name, presence: true
   validates :description, presence: true, length: { minimum: 10 }
