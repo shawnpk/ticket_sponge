7e0342dca58abec03be1e2f471ea52c883b97881
Shawn Kearney <shawnpk@ineptsoftware.com>
2017-04-14 08:20:11 -0700

    Add ability for admin to assign roles for new users

--- spec/features/admin/managing_roles_spec.rb
+++ spec/features/admin/managing_roles_spec.rb
@@ -25,4 +25,18 @@ RSpec.describe 'Admin can manage a user\'s roles' do
     expect(page).to have_content 'Chrome: Viewer'
     expect(page).to have_content 'Atom.io: Manager'
   end
+
+  scenario 'when assigning foles to a new user' do
+    visit new_admin_user_path
+
+    fill_in 'Email', with: 'newuser@ticket_sponge.com'
+    fill_in 'Password', with: 'password'
+
+    select 'editor', from: 'Chrome'
+    click_button 'Create User'
+    click_link 'newuser@ticket_sponge.com'
+
+    expect(page).to have_content 'Chrome: Editor'
+    expect(page).not_to have_content 'Atom.io'
+  end
 end
